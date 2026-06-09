9648fe7329cd2133f4ffcc817c76766e70cabae4
Abhyuday Gautam <abgautam1903@gmail.com>
2026-06-09 23:08:27 +0530

    Day 1 Completed

--- Day-1/problem_1.cpp
+++ Day-1/problem_1.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
     int N;
-    cout<<"Enter the value of N: ";
+    cout<<"Enter the value of N: ";  // Input a number N from the user
     cin>>N;
-    int sum = N*(N+1)/2;
+    int sum = N*(N+1)/2; // Calculate the sum of first N natural numbers using the formula N*(N+1)/2
     cout<<"The sum of first "<<N<<" natural numbers is: "<<sum<<endl;
     return 0;
 }
--- Day-1/problem_2.cpp
+++ Day-1/problem_2.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main()
 {
     int num;
-    cout<<"Enter the number: ";
+    cout<<"Enter the number: "; // Input a number from the user
     cin>>num;
     for(int i=1;i<=10;i++)
     {
-        cout<<num<<" x "<<i<<" = "<<num*i<<endl;
+        cout<<num<<" x "<<i<<" = "<<num*i<<endl; // Printing the table
     }
     return 0;
 }
